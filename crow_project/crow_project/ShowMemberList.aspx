091331bcf6decc079f7bf6e3960476af18dd80ea
PC00089201001\user <xm105rwa@gmail.com>
2018-05-25 17:44:26 +0900

    従業員一覧画面の制作

---
+++ crow_project/crow_project/ShowMemberList.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ShowMemberList.aspx.cs" Inherits="UC2.ShowMemberList" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="ShowMemberList.css">
+</head>
+<body>
+    <div align="center">
+    <form id="form1" runat="server">
+        <h1>従業員一覧</h1>
+        <asp:Table ID="Table1" runat="server" GridLines="Both">
+            <asp:TableRow runat="server">
+                <asp:TableHeaderCell runat="server">従業員コード</asp:TableHeaderCell>
+                <asp:TableHeaderCell runat="server">氏名</asp:TableHeaderCell>
+                <asp:TableHeaderCell runat="server">氏名(フリガナ)</asp:TableHeaderCell>
+                <asp:TableHeaderCell runat="server">性別</asp:TableHeaderCell>
+                <asp:TableHeaderCell runat="server">生年月日</asp:TableHeaderCell>
+                <asp:TableHeaderCell runat="server">所属部署</asp:TableHeaderCell>
+                <asp:TableHeaderCell runat="server">入社日</asp:TableHeaderCell>
+                <asp:TableCell runat="server">&emsp;&emsp;&emsp;</asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+        <asp:HyperLink ID="BackMenu" runat="server" NavigateUrl="aaa.aspxs">メニュー画面へ</asp:HyperLink>
+    </form>
+    </div>
+</body>
+</html>
---
+++ crow_project/crow_project/ShowMemberList.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using crow_project;
+
+namespace UC2 {
+    public partial class ShowMemberList : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            using (TransMng trn = new TransMng()) {
+
+                Dao dao = new Dao();
+                List<string> member;
+
+                member = dao.Show();
+
+                for (int i = 0; i < member.Count / 7; i++) {
+                    TableRow row = new TableRow();
+                    Table1.Rows.Add(row);
+                    row.Cells.AddRange(new TableCell[] { new TableCell(), new TableCell(), new TableCell(), new TableCell(), new TableCell(), new TableCell(), new TableCell(), new TableCell() });
+                    for (int j = 0; j <= 7; j++) {
+                        if (j == 7) {
+                            Button deleteButton = new Button();
+                            deleteButton.Text = "削除";
+                            deleteButton.ID = member[i * 7];
+                            deleteButton.Click += new EventHandler(Remover_ButtonClick);
+                            row.Cells[j].Controls.Add(deleteButton);
+                            break;
+                        }
+                        row.Cells[j].Text = member[j + (i * 7)];
+                        row.Cells[j].BorderStyle = BorderStyle.Solid;
+
+                    }
+                }
+            }
+        }
+
+        protected void Remover_ButtonClick(object sender, EventArgs e) {
+            string code = ((Button)sender).ID;
+            Session.Add("delete", code);
+            Response.Redirect("Remover.aspx");
+        }
+    }
+}

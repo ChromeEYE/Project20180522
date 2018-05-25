20e8c71ab9c5cc19a271a94caab3988d1f7bf1a1
PC00089201001\user <xm105rwa@gmail.com>
2018-05-25 17:16:49 +0900

    一覧表示制作

---
+++ crow_project/crow_project/Dao.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Collections.Generic;
+using crow_project;
+
+
+namespace UC2 {
+    public class Dao {
+        /// <summary>コネクション</summary>
+        private readonly SqlConnection con;
+        /// <summary>トランザクション</summary>
+        private readonly SqlTransaction trn;
+        /// <summary>DAO（従業員テーブル）</summary>
+        public Dao() {
+            con = TransMng.Transaction.Connection;
+            trn = TransMng.Transaction;
+        }
+
+        public  List<string> Show() {
+
+            List<string> member = new List<string>() { "1","2","3","4","5","6","7","8","9","10","11","12","13","14"};
+
+            using (SqlCommand cmd = new SqlCommand("SELECT *FROM m_employee", con, trn)) {
+            //    using (SqlDataReader reader = cmd.ExecuteReader()) {
+            //        while (reader.Read()) {
+            //            member.Add(reader["emp_cd"].ToString());
+            //            member.Add(reader["emp_cd"].ToString());
+            //            member.Add(reader["emp_cd"].ToString());
+            //            member.Add(reader["emp_cd"].ToString());
+            //            member.Add(reader["emp_cd"].ToString());
+            //            member.Add(reader["emp_cd"].ToString());
+            //        }
+            //    }
+                return member;
+            }
+        }
+        public bool Remover(string code) { return true; }
+    }
+}
